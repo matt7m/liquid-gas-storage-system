eb55717ded897ed96a60d3e0e70eb3a59ec494fa
Mateusz Murawski <matim1324@gmail.com>
2025-08-31 20:12:59 +0200

    Modify controllers for document management system endpoints

--- backend/src/main/java/com/lpgdepot/backend/controller/GasDepotController.java
+++ backend/src/main/java/com/lpgdepot/backend/controller/GasDepotController.java
@@ -3,13 +3,12 @@ package com.lpgdepot.backend.controller;
 import com.lpgdepot.backend.entity.GasDepot;
 import com.lpgdepot.backend.entity.GasTank;
 import com.lpgdepot.backend.entity.GasType;
+import com.lpgdepot.backend.exception.NotFoundException;
 import com.lpgdepot.backend.service.GasDepotService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,34 +22,91 @@ public class GasDepotController {
         this.gasDepotService = gasDepotService;
     }
 
-    @GetMapping("/text")
-    public ResponseEntity<String> text(){
-        String depots = "everything is good";
+//    @GetMapping("/text")
+//    public ResponseEntity<String> text(){
+//        String depots = "everything is good";
+//
+//        return new ResponseEntity<>(depots, HttpStatus.OK);
+//    }
 
-        return new ResponseEntity<>(depots, HttpStatus.OK);
-    }
-
-    @GetMapping("/status")
+    @GetMapping("/list")
     public ResponseEntity<List<GasDepot>> getAllDepot(){
-//        try {
-            List<GasDepot> depots = gasDepotService.getDepotStatus();
+        try {
+            List<GasDepot> depots = gasDepotService.getDepotList();
 
             if (depots.isEmpty()) {
                 return new ResponseEntity<>(HttpStatus.NO_CONTENT);
             }
 
-            //System.out.println(depots.get(1).getName());
-            //System.out.println(depots.get(1).getCapacity());
             return new ResponseEntity<>(depots, HttpStatus.OK);
 
-//        } catch (Exception e){
-//            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-//        }
+        } catch (Exception e){
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/gas_types")
+    public ResponseEntity<List<GasType>> getGasTypes() {
+        List<GasType> types = gasDepotService.getGastTypeList();
+        return new ResponseEntity<>(types, HttpStatus.OK);
     }
 
     @GetMapping("/gas_tanks")
     public ResponseEntity<List<GasTank>> gatGasTanks() {
         List<GasTank> tanks = gasDepotService.getTanksList();
         return new ResponseEntity<>(tanks, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<GasDepot> getGasDepotById(@PathVariable("id") int id){
+        try {
+            GasDepot depot = gasDepotService.getGasDepotById(id);
+            System.out.println(depot.getGasType());
+
+            return new ResponseEntity<>(depot, HttpStatus.OK);
+        } catch (NotFoundException e) {
+            return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+        } catch (Exception e){
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/currentState/{id}")
+    public ResponseEntity<Long> getGasDepotStatusById(@PathVariable("id") int id){
+        try {
+            Long result = gasDepotService.getGasDepotCurrentStatus(id);
+
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        } catch (NotFoundException e) {
+            return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+        } catch (Exception e){
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/load")
+    public ResponseEntity<String> loadGasDepot(@RequestParam(name = "id") int id, @RequestParam(name = "updateValue") Long updateValue){
+        try {
+            int res = gasDepotService.loadGasDepot(id, updateValue);
+
+            if (res == 1) {
+                return new ResponseEntity<>("Updated gas depot - " + id, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>("Not enough space on gas depot - " + id, HttpStatus.OK);
+            }
+
+        } catch (NotFoundException e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PutMapping("/unload")
+    public ResponseEntity<String> unloadGasDepot(@RequestParam(name = "id") int id, @RequestParam(name = "updateValue") Long updateValue){
+        int res = gasDepotService.unloadGasDepot(id, updateValue);
+        if (res == 1) {
+            return new ResponseEntity<>("Unloaded gas depot successfully", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Not enough liquid gas on depot", HttpStatus.OK);
+        }
     }
 }
--- backend/src/main/java/com/lpgdepot/backend/controller/GasManagementController.java
+++ backend/src/main/java/com/lpgdepot/backend/controller/GasManagementController.java
@@ -1,13 +1,13 @@
 package com.lpgdepot.backend.controller;
 
 import com.lpgdepot.backend.entity.GasManagement;
+import com.lpgdepot.backend.exception.NotFoundException;
 import com.lpgdepot.backend.service.GasDepotService;
 import com.lpgdepot.backend.service.GasManagementService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,7 +24,7 @@ public class GasManagementController {
         this.gasDepotService = gasDepotService;
     }
 
-    @GetMapping("/docs")
+    @GetMapping("/docs/list")
     public ResponseEntity<List<GasManagement>> getAllDocuments() {
         List<GasManagement> docs = gasManagementService.getAllGasManagementDocs();
         if (docs.isEmpty()) {
@@ -33,4 +33,38 @@ public class GasManagementController {
 
         return new ResponseEntity<>(docs, HttpStatus.OK);
     }
+
+    @GetMapping("/doc")
+    public ResponseEntity<GasManagement> getDocumentByArc(@RequestParam("arc") String arc) {
+        try {
+            GasManagement doc = gasManagementService.getManagementDocByARC(arc);
+
+            return new ResponseEntity<>(doc, HttpStatus.OK);
+        } catch (NotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/new_doc")
+    public ResponseEntity<String> createGasManagement(@Valid @RequestBody GasManagement gasManagement) {
+        try {
+            gasManagementService.addGasManagementDoc(gasManagement);
+
+            return new ResponseEntity<>("New management doc added.", HttpStatus.CREATED);
+        } catch (Exception e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping("/doc_del")
+    public ResponseEntity<String> deleteGasManagement(@RequestParam("arc") String arc) {
+        try {
+            gasManagementService.deleteManagementDoc(arc);
+        } catch (NotFoundException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>("Document deleted from system successfully.", HttpStatus.OK);
+    }
+
 }
